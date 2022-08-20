b1be9c4a6530a8865a898ee9e821b52c6e3874c4
Muhammad Syahrul <noturaun@gmail.com>
2022-08-21 00:47:31 +0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Aboreto">
+</svelte:head>
+
+<style>
+    body {
+        background-color: azure;
+    }
+
+    h1, p {
+        font-family: 'Aboreto', serif;
+    }
+
+    .container {
+        height: 100vh;
+        margin: auto;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+    }
+</style>
+
+
+<div class="container">
+    <slot>
+    </slot>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+
+<div class="hello">
+    <h1>Halo, kami Tim Elang</h1>
+    <p>Built with ❤️ and Docs from <a href="https://kit.svelte.dev">kit.svelte.dev</a></p>    
+</div>
+
+
+<style>
+    h1 {
+        font-family: 'Aboreto', serif;
+    }
+
+    .hello {
+        grid-column-start: 2;
+        grid-column-end: 2;
+        grid-row-start: 2;
+        grid-row-end: 2;
+        align-items: start;
+    }
+</style>
